ad069c5da6c5de10d7790da796ea684e1b02bc88
Petter Karlsson <petter.karlsson@cmeducations.se>
2022-10-11 10:43:47 +0200

    last fix

--- js/menu.js
+++ js/menu.js
@@ -3,9 +3,10 @@ const $menyLayer1 = document.getElementById("menu-layer1");
 const $x = document.getElementById("x");
 const $transLayer = document.getElementById("trans-layer");
 const $menuContainer = document.getElementById("menu-container");
-const speed = 30;
+const $arrow = document.getElementById("arrow");
+const speed = 20;
 let step = speed;
-let count = -1000;
+let count = -2000;
 const $menuItems = [
     document.getElementById("menu-item1"),
     document.getElementById("menu-item2"),
@@ -30,20 +31,17 @@ $menuItems2.map((items) => {
 
 $menyButton.addEventListener("click", () => {
     step = speed;
-
     $menuContainer.style.zIndex = 20;
     $menyLayer1.style.display = "block";
     $transLayer.style.display = "block";
-    $transLayer.style.left = "0px";
     window.requestAnimationFrame(playAnimation);
 });
 $x.addEventListener("click", () => {
     step = -speed;
     $menyLayer1.style.display = "block";
     $transLayer.style.display = "none";
-    $menuContainer.style.right = "-800px";
-    $menuContainer.style.zIndex = 20;
-
+    $menuContainer.style.right = "-1500px";
+    $menuContainer.style.zIndex = 200;
 });
 
 $menuItems.forEach((items, i) => {
@@ -61,10 +59,16 @@ function playAnimation() {
         }
     }
     if (step === -speed) {
-        if (count < 800) {
+        if (count > -2000) {
             count += step;
             $menuContainer.style.right = count + "px";
         }
     }
     window.requestAnimationFrame(playAnimation);
 }
+$arrow.addEventListener("click", () => {
+    window.scrollTo({
+        top: window.innerHeight,
+        behavior: "smooth",
+    });
+});
